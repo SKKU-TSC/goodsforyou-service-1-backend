654d56705721aee666871362392eb1f1a6bf2458
taeju-moon <pp2lycee@naver.com>
2022-08-23 23:33:01 +0900

    a

--- controllers/akinator.js
+++ controllers/akinator.js
@@ -9,25 +9,26 @@ const getResult = async (req, res) => {
       featureName: options[0],
     });
     let usingItems = [...rootFeature.defaultItems];
-    if (options.length < 1){
+    if (options.length < 1) {
       res.status(400).json({
         status: 'fail',
         message: 'option 없이는 추천할 수 없습니다.',
-      })} else {
-        for (let i = 1; i < options.length; i++) {
-            usingItems = usingItems.filter((item) => {
-              const usingFeature = await FeatureCorpus.findOne({
-                  featureName: options[i][0]
-              });
-              if (options[i][1]){
-                return usingFeature.defaultItems.indexOf(item) >=0;
-              } else {
-                return usingFeature.defaultItems.indexOf(item) === -1;
-              }
-            });
+      });
+    } else {
+      for (let i = 1; i < options.length; i++) {
+        usingItems = usingItems.filter(async (item) => {
+          const usingFeature = await FeatureCorpus.findOne({
+            featureName: options[i][0],
+          });
+          if (options[i][1]) {
+            return usingFeature.defaultItems.indexOf(item) >= 0;
+          } else {
+            return usingFeature.defaultItems.indexOf(item) === -1;
           }
-          res.status(200).json({status:"success", usingItems})
-      };
+        });
+      }
+      res.status(200).json({ status: 'success', usingItems });
+    }
   } catch (err) {
     res.status(400).json({ status: 'fail', err });
   }
